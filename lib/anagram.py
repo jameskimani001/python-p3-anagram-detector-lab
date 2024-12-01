325a94dbeca1efab47e97da82428303bd9290c5a
jameskimani001 <jameskim092t@gmail.com>
2024-12-01 14:50:28 +0300

    anagram complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+# lib/anagram.py
+class Anagram:
+    def __init__(self, word):
+        # Initialize with the word for which anagrams will be found
+        self.word = word
+
+    def match(self, possible_anagrams):
+        # Find all words in possible_anagrams that are anagrams of self.word
+        return [candidate for candidate in possible_anagrams if sorted(candidate) == sorted(self.word)]
